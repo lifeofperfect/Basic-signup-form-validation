8dbe93bf0f15743ab6cba26b6688c6148a98a51c
lifeofperfect <perfect.igbadumhe@gmail.com>
2020-05-17 03:05:04 +0100

    optimized javascript

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
 //show error
+/*
 function showError(input, message) {
   const formControl = input.parentElement;
   formControl.className = "form-control error";
@@ -51,4 +52,74 @@ form.addEventListener("submit", (e) => {
   } else {
     showSuccessss(password2);
   }
+}); */
+
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+//show success
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+const capitalField = (input) => {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+};
+
+const checkRequired = (inputArr) => {
+  inputArr.forEach((input) => {
+    if (input.value.trim() === "") {
+      showError(input, `${capitalField(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+};
+
+const checkLength = (input, min, max) => {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${capitalField(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${capitalField(input)} must be less than ${max} characters`
+    );
+  }
+};
+
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (re.test(email.value.trim())) {
+    showSuccess(email);
+  } else {
+    showError(email, "Email is not valid");
+  }
+}
+
+const checkPassword = (item1, item2) => {
+  if (item1.value != item2.value) {
+    showError(item1, "Password do not match");
+    showError(item2, "Password do not match");
+  } else {
+    showSuccess(item1);
+    showSuccess(item2);
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 10);
+  checkLength(password, 7, 20);
+  isValidEmail(email);
+  checkPassword(password, password2);
 });
